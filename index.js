02b90e8e87d02ddfb6a39814c005d93c844b7393
Nigora Bobayarova <57304108+nigora8@users.noreply.github.com>
2023-07-11 01:26:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const minuteEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 const ampmEl = document.getElementById("ampm");
 
-function updateClock(){
+function updateClock() {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-    let ampm = "AM";
+    let ampm = "PM";
 
-    if(h > 12) {
+    if (h > 12) {
         h = h - 12;
         ampm = "PM";
     }
-
+    else {
+        ampm = "AM"
+    }
+   
     h = h<10 ? "0" + h : h;
     m = m<10 ? "0" + m : m;
     s = s<10 ? "0" + s : s;
@@ -22,7 +25,7 @@ function updateClock(){
     minuteEl.innerText = m;
     secondsEl.innerText = s;
     ampmEl.innerText = ampm;
-    setTimeout(()=>{
+    setTimeout(() => {
         updateClock();
 
     }, 1000);
